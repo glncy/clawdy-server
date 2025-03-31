190e94c358f23d94ebfef51c8aa6b135650ceba6
Glency Tirao <glency.a.tirao@gmail.com>
2025-03-31 15:03:43 +0800

    chore: init cloudflare workers and hono server

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,16 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.get("/health-check", (ctx) => {
+  return ctx.json({
+    status: "ok",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
