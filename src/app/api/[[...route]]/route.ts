f42535987af9f33f2b5743e977150d95766228b1
Glency Tirao <glency.a.tirao@gmail.com>
2025-04-24 02:57:00 +0800

    chore: remove edge

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,11 +1,1 @@
-// TODO: Make a workaround to block the route from being used in Cloudflare environment
-// import { CLOUDFLARE_ENABLED } from "@/constants";
-
-// if (CLOUDFLARE_ENABLED) {
-//   throw new Error(
-//     "You are on Cloudflare Environment. Please use /cf route instead of / route."
-//   );
-// }
-
-export const runtime = "edge";
 export * from "@/hono/route";
