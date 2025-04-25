122f4d4e0f45e395063842beb2e418a7dd32c9a7
Glency Tirao <glency.a.tirao@gmail.com>
2025-04-25 17:55:51 +0800

    build: staging build script

---
+++ scripts/vercel_build/vercel_staging_build.js
@@ -0,0 +1,31 @@
+/**
+ * This script controls which Git branches are allowed to build in the Vercel environment.
+ * It exits with code 0 to proceed with the build, or 1 to cancel the build.
+ */
+
+const buildUtils = require("./build_utils");
+const { isMatched, currentGitBranch } = buildUtils;
+
+// List of branches that are allowed to build
+const allowedBranches = [
+  "staging"
+];
+
+/**
+ * Main function to determine if the build should proceed
+ */
+const main = () => {
+  console.log(`Current Git branch: ${currentGitBranch}`);
+
+  if (isMatched(currentGitBranch, allowedBranches)) {
+    // Proceed with the build
+    console.log("✅ - Build can proceed");
+    process.exit(1); // Exit with success code to proceed with the build
+  } else {
+    // Don't build
+    console.log("🛑 - Build ignored");
+    process.exit(0); // Exit with failure code to cancel the build
+  }
+};
+
+main();
